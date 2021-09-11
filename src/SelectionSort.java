8e0bbfa0ad0a3ea7123a5e5fe79b5af1587ecd17
Castro <jcastroi@amazon.com>
2021-09-11 12:54:56 -0400

    finished i think

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,9 +1,11 @@
-/**
- * 
- */
-//package com.allendowney.thinkdast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.io.FileWriter;
+
 
 /**
  * @author downey
@@ -43,14 +45,81 @@ public class SelectionSort {
 			swapElements(array, i, j);
 		}
 	}
+	
+	/*public List<Integer> generateRandomArray(int n){
+	    ArrayList<Integer> list = new ArrayList<Integer>(n);
+	    Random random = new Random();
+	    
+	    n = 100;
+	    
+	    for (int i = 0; i < n; i++)
+	    {
+	        list.add(random.nextInt(999999));
+	    }
+	   return list;
+	}  
+	*/
+	
+	
+	 public static int[] getRandomArray(int howMany) 
+	 {
+	        int[] returnArray = new int[howMany]; // Assume size >= 0
+	        Random r = new Random();
+	        for (int i = 0; i < howMany; i++) 
+	        {
+	        	returnArray[i] = r.nextInt(999999);   
+	            //System.out.print(returnArray[i] + " ");
+	            for (int n = 1; n <= returnArray.length; n++) 
+	            {
+	                for (int j = n + 1; j < returnArray.length; j++) 
+	                {
+	                    int tmp = 0;
+	                    if (returnArray[j] > returnArray[n]) 
+	                    {
+	                        tmp = returnArray[n];
+	                        returnArray[n] = returnArray[j];
+	                        returnArray[j] = tmp;
+	                    }
+	                }
+	            }
+	        }
+	        return returnArray;
+	    }
+	
+	
+	
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		int[] array = {2, 5, 6, 1, 3};
-		selectionSort(array);
-		System.out.println(Arrays.toString(array));
-	}
+	public static void main(String[] args) throws Exception 
+	{
+		//int[] array = {2, 5, 6, 1, 3};
+		int howMany = 250;
+		int[] myArray = getRandomArray(howMany);		
+		
+		final long startTime = System.currentTimeMillis();
+		
+		selectionSort(myArray);
+		
+		final long endTime = System.currentTimeMillis();
+		
+		//System.out.println(startTime);
+		//System.out.println(endTime);
+		
+		System.out.println("Current Time in milliseconds = " + (endTime - startTime)); //print out how long in milliseconds it took for selectionSort to sort through all the numbers
+	     //System.out.println(System.currentTimeMillis());
+		
+		//System.out.println(Arrays.toString(myArray));
+		
+		FileWriter write2file = new FileWriter("docs\\arraylist"); 
+		
+		 int n = myArray.length;
+	      for (int i = 0; i < n; i++) {
+	         write2file.write(myArray[i] + "\n"); //write array numbers in order to the text document arraylist
+	      }
+	      write2file.close();
+	   }
+
 
 }
