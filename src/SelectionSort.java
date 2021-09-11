1d542af848205a62007c07f3480ab03a3746372e
Joe Castro <joecastroiv@hotmail.com>
2021-09-10 22:32:50 -0400

    work from home

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -3,7 +3,13 @@
  */
 //package com.allendowney.thinkdast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+
 
 /**
  * @author downey
@@ -43,14 +49,60 @@ public class SelectionSort {
 			swapElements(array, i, j);
 		}
 	}
+	
+	/*public List<Integer> generateRandomArray(int n){
+	    ArrayList<Integer> list = new ArrayList<Integer>(n);
+	    Random random = new Random();
+	    
+	    n = 100;
+	    
+	    for (int i = 0; i < n; i++)
+	    {
+	        list.add(random.nextInt(999999));
+	    }
+	   return list;
+	}  
+	*/
+	
+	
+	 public static int[] getRandomArray(int howMany) {
+	        int[] returnMe = new int[howMany]; // Assume size >= 0
+	        Random rand = new Random();
+	        for (int i = 0; i < howMany; i++) {
+	            returnMe[i] = rand.nextInt(20) + 1;   //Integer.MAX_VALUE
+	            System.out.print(returnMe[i] + " ");
+	            for (int n = 1; n <= returnMe.length; n++) {
+	                for (int j = n + 1; j < returnMe.length; j++) {
+	                    int tmp = 0;
+	                    if (returnMe[n] > returnMe[j]) {
+	                        tmp = returnMe[n];
+	                        returnMe[n] = returnMe[j];
+	                        returnMe[j] = tmp;
+	                    }
+	                }
+	            }
+	        }
+	        return returnMe;
+	    }
+	
+	
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] array = {2, 5, 6, 1, 3};
-		selectionSort(array);
-		System.out.println(Arrays.toString(array));
+		//int[] array = {2, 5, 6, 1, 3};
+		int howMany = 5;
+		int[] myArray = getRandomArray(howMany);
+		
+		selectionSort(myArray);
+		
+		 System.out.println("Current Time in milliseconds = " + System.currentTimeMillis());
+	     //System.out.println(System.currentTimeMillis());
+		
+		System.out.println(Arrays.toString(myArray));
+		
 	}
 
 }
